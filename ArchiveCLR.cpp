9532cd8f576418215bd6b1e0f436a13d58a7d0f2
Alex <66972768+PlahoninAlexandr@users.noreply.github.com>
2021-08-03 11:45:06 +0300

    Add files via upload

--- ArchiveCLR.cpp
+++ ArchiveCLR.cpp
@@ -1,30 +1,27 @@
 #include "pch.h"
 #include "ArchiveCLR.h"
 
-void ArchiveCLR::ArchiveFunctionWrapper::extractArchive(HWND hWnd, wstring str, string& path, string& outname) {
-	archive->extractArchive(hWnd, str, path, outname);
+void ArchiveCLR::ArchiveFunctionWrapper::extractArchive(wstring str, string& path, string& outname) {
+	archive->extractArchive(str, path, outname);
 }
 
 void ArchiveCLR::ArchiveFunctionWrapper::addFileInArchive() {
 	archive->addFileInArchive();
 }
 
-void ArchiveCLR::ArchiveFunctionWrapper::close(HWND hWnd1, HWND hWnd2) {
-	archive->close(hWnd1, hWnd2);
-}
-
-void ArchiveCLR::ArchiveFunctionWrapper::writeArchiveSingle(HWND hWnd, wstring str, string path, vector<string> argv, string outname) {
-	archive->writeArchiveSingle(hWnd, str, path, argv, outname);
+void ArchiveCLR::ArchiveFunctionWrapper::writeArchiveSingle(wstring str, string path, vector<string> argv, string outname) {
+	archive->writeArchiveSingle(str, path, argv, outname);
 }
 
 void ArchiveCLR::ArchiveFunctionWrapper::writeArchiveDirectory(wstring str, string path, vector<string> argv, string outname) {
 	archive->writeArchiveDirectory(str, path, argv, outname);
 }
 
-void ArchiveCLR::ArchiveFunctionWrapper::selectFile(HWND& hWnd, wstring& str) {
-	archive->selectFile(hWnd, str);
+void ArchiveCLR::ArchiveFunctionWrapper::selectFile(string& str) {
+	wstring ws(str.begin(), str.end());
+	archive->selectFile(ws);
 }
 
-void ArchiveCLR::ArchiveFunctionWrapper::selectArchive(HWND& hWnd, wstring& str) {
-	archive->selectArchive(hWnd, str);
+void ArchiveCLR::ArchiveFunctionWrapper::selectArchive(wstring& str) {
+	archive->selectArchive(str);
 }
--- ArchiveCLR.h
+++ ArchiveCLR.h
@@ -5,19 +5,18 @@ using namespace System;
 
 namespace ArchiveCLR {
 	public ref class ArchiveFunctionWrapper {
-		ArchiveFunction* archive;
+		IAbstractArchive* archive;
 	public:
 		ArchiveFunctionWrapper() {
 			unique_ptr<ArchiveFactory> f1(new ArchiveFactory);
-			unique_ptr<IAbstractArchive> archive(f1->CreateProductA());
+			this->archive = f1->CreateProductA();
 		};
 
-		void extractArchive(HWND hWnd, wstring str, string& path, string& outname);
+		void extractArchive(wstring str, string& path, string& outname);
 		void addFileInArchive();
-		void close(HWND hWnd1, HWND hWnd2);
-		void writeArchiveSingle(HWND hWnd, wstring str, string path, vector<string> argv, string outname);
+		void writeArchiveSingle(wstring str, string path, vector<string> argv, string outname);
 		void writeArchiveDirectory(wstring str, string path, vector<string> argv, string outname);
-		void selectFile(HWND& hWnd, wstring& str);
-		void selectArchive(HWND& hWnd, wstring& str);
+		void selectFile(string& str);
+		void selectArchive(wstring& str);
 	};
 }
